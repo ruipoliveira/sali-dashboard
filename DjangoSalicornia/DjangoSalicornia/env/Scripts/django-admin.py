930493f775e0a46ec7449803dcd8ef360b7c25e5
RuiOliveira <RuiOliveira@DESKTOP-8RS6ENT>
2016-09-06 14:12:08 +0100

    add first version

---
+++ DjangoSalicornia/DjangoSalicornia/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\TESE-SALICORNIA\ThesisSalicornia-web\DjangoSalicornia\DjangoSalicornia\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
