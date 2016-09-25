7e1962693785ff9774c14ec1ebbb34add39a9059
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-09-25 19:59:34 +0100

    já tem sistema de login e organização do template base. proximo passo: possibilidade de adicionar novos users e alterar profile

--- DjangoSalicornia/DjangoSalicornia/DjangoSalicornia/urls.py
+++ DjangoSalicornia/DjangoSalicornia/DjangoSalicornia/urls.py
@@ -6,41 +6,36 @@ from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
 
-import app.forms
+from django.contrib import admin
+from app.views import *
+from django.contrib.auth.views import login
+
+from django.contrib.auth.decorators import user_passes_test
+
 import app.views
 
 # Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
+from django.conf.urls import include
+from django.contrib import admin
 # admin.autodiscover()
+
+login_forbidden =  user_passes_test(lambda u: u.is_anonymous(), 'home')
 
 urlpatterns = [
     # Examples:
-    url(r'^$', app.views.home, name='home'),
-    url(r'^contact$', app.views.contact, name='contact'),
-    url(r'^about', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+    
+    url(r'^$', login_forbidden(login), name="login"),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^home/$', home, name='home'),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', login),
+    url(r'^logout/$', logout_page),
+
+    url(r'^addusers/$', app.views.newuser),
+
+    url(r'^novo/$', app.views.addNewUser),
+
+    url(r'^changeprofile/$', app.views.changeprofile),
+
+    url(r'^admin/', include(admin.site.urls)),
 ]
