243d0cce48bb9a687e7ff25e455bc1c12e57512d
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-02-11 00:15:07 +0000

    new up

--- DjangoSalicornia/DjangoSalicornia/app/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-from django.forms import ModelForm
-from django.contrib.auth.models import User
-
-# Create your models here.
-
