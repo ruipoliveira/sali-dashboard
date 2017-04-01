b839eafce36a8a37459118d45754c23f219315a4
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-04-02 00:51:09 +0100

    up!!

--- saliDashboard/saliDashboard/settings.py
+++ saliDashboard/saliDashboard/settings.py
@@ -126,6 +126,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_URL = '/static/'
