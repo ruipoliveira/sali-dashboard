97c97dc6c79eebc5d94bcad2e2a161bffd4967fe
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-07-21 15:56:44 +0100

    d

--- saliDashboard/saliapp/templatetags/tag_library.py
+++ saliDashboard/saliapp/templatetags/tag_library.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 @register.filter()
 def to_int_one_less(value):
-    return int(value) - 1
+    return long(value) - 1
 
 
 @register.filter()
 def to_int_one_more(value):
-    return int(value) + 1
+    return long(value) + 1
