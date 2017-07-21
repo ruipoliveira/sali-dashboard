07091562ba2f56debb5592acecbabb050fe9fa22
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-07-21 15:59:53 +0100

    fixed scale

--- saliDashboard/saliapp/templatetags/tag_library.py
+++ saliDashboard/saliapp/templatetags/tag_library.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 @register.filter()
 def to_int_one_less(value):
-    return float(value) - 1
+    return float(value) + 1
 
 
 @register.filter()
 def to_int_one_more(value):
-    return float(value) + 1
+    return float(value) - 1
