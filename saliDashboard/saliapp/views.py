4a2cd80d7755e62eb8a059e27fc20abb5bd6c960
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-07-30 02:08:11 +0100

    reversed allarms

--- saliDashboard/saliapp/views.py
+++ saliDashboard/saliapp/views.py
@@ -210,7 +210,7 @@ def home(request):
          'CMregistrations': ControllerModule.objects.all().count(),
          'totalReadings': Reading.objects.all().count(),
          'allReadings': cenas,
-         'allAlarms': Alarms.objects.all().reverse()
+         'allAlarms': Alarms.objects.all()
          # 'obj': CPU.objects.all()
          })
 
